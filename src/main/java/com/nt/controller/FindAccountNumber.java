32cda3e110402bef36095e18a191639cbaed5ae8
Amps11$$ <sdigamarthi@gmail.com>
2023-02-05 20:10:20 +0530

    added sysout

--- src/main/java/com/nt/controller/FindAccountNumber.java
+++ src/main/java/com/nt/controller/FindAccountNumber.java
@@ -11,6 +11,7 @@ public class FindAccountNumber {
 	public static String getAccountNumber(String accountNo) {
 		Optional<String> accno = listofAccounts.stream().filter(b -> b.equals(accountNo)).findFirst();
 
+		System.out.println("hello world from account");
 		return accno.isPresent() ? "Given account number is identified" : "Account number does not exist";
 	}
 
